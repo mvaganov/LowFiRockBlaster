bb2eb7b34ff5edf61a06d5ebc761de9767ea21ac
Michael Vaganov <mvaganov@hotmail.com>
2025-10-01 19:12:58 +0900

    interfaces

---
+++ src/MrV/GameEngine/IDrawable.cs
@@ -0,0 +1,8 @@
+﻿using MrV.CommandLine;
+
+namespace MrV.GameEngine {
+	public interface IDrawable {
+		public bool IsVisible { get; set; }
+		public void Draw(GraphicsContext canvas);
+	}
+}
---
+++ src/MrV/GameEngine/IGameObject.cs
@@ -0,0 +1,10 @@
+﻿using MrV.Geometry;
+
+namespace MrV.GameEngine {
+	public interface IGameObject : IDrawable {
+		public string Name { get; set; }
+		public Vec2 Position { get; set; }
+		public bool IsActive { get; set; }
+		public void Update();
+	}
+}
