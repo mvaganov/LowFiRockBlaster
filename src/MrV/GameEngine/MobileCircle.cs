63b2dda2c85c70d02369bb47baaba86b75063d11
Michael Vaganov <mvaganov@hotmail.com>
2026-01-05 18:28:06 +0900

    simplify mobile circle, add to vec2

--- src/MrV/GameEngine/MobileCircle.cs
+++ src/MrV/GameEngine/MobileCircle.cs
@@ -3,36 +3,14 @@ using MrV.Geometry;
 
 namespace MrV.GameEngine {
 	public class MobileCircle : MobileObject {
-		private Circle Circle;
+		public Circle Circle;
 		public override Vec2 Position { get => Circle.Center; set => Circle.Center = value; }
-		public override Vec2 Direction { get => Velocity.Normalized(); set { } }
-		public float Radius { get => Circle.Radius; set => Circle.Radius = value; }
-		public static bool DebugShowVelocity = false;
-		public MobileCircle(Circle circle) {
-			Circle = circle;
-		}
-		public virtual void Copy(MobileCircle other) {
-			base.Copy(other);
-			Circle = other.Circle;
-		}
-		public Circle GetCollisionBoundingCircle() => Circle;
+		public override Vec2 Direction { get => Velocity.Equals(Vec2.Zero) ? Vec2.ZeroDegrees : Velocity.Normalized(); set { } }
+		public MobileCircle(Circle circle) { Circle = circle; }
 		public override void Draw(GraphicsContext canvas) {
-			if (!_active) return;
+			if (!_active) { return; }
 			canvas.SetColor(Color);
 			canvas.DrawCircle(Circle);
-			if (DebugShowVelocity) {
-				ShowDebugVelocity(canvas);
-			}
-		}
-
-		private void ShowDebugVelocity(GraphicsContext graphicsContext) {
-			float speed = Velocity.Length();
-			if (speed == 0) { return; }
-			Vec2 dir = Velocity / speed;
-			Vec2 start = Position + dir * Radius;
-			Vec2 end = start + Velocity;
-			graphicsContext.SetColor(Color);
-			graphicsContext.DrawLine(start, end, 1);
 		}
 	}
 }
--- src/MrV/Geometry/Vec2.cs
+++ src/MrV/Geometry/Vec2.cs
@@ -2,6 +2,16 @@
 
 namespace MrV.Geometry {
 	public struct Vec2 {
+		public static readonly Vec2 Zero = (0, 0);
+		public static readonly Vec2 ZeroDegrees = (1, 0);
+		public static readonly Vec2 One = (1, 1);
+		public readonly static Vec2 Half = (1f / 2, 1f / 2);
+		public readonly static Vec2 Max = (float.MaxValue, float.MaxValue);
+		public readonly static Vec2 Min = (float.MinValue, float.MinValue);
+		public readonly static Vec2 UnitX = (1, 0);
+		public readonly static Vec2 UnitY = (0, 1);
+		public readonly static Vec2 NaN = (float.NaN, float.NaN);
+		public const float epsilon = 1f / (1 << 10);
 		public float X, Y;
 		public Vec2(float x, float y) { X = x; Y = y; }
 		public static Vec2 operator +(Vec2 a, Vec2 b) => new Vec2(a.X + b.X, a.Y + b.Y);
@@ -9,6 +19,13 @@ namespace MrV.Geometry {
 		public static Vec2 operator *(Vec2 a, Vec2 b) => new Vec2(a.X * b.X, a.Y * b.Y);
 		public static Vec2 operator /(Vec2 a, Vec2 b) => new Vec2(a.X / b.X, a.Y / b.Y);
 		public static implicit operator Vec2((float x, float y) tuple) => new Vec2(tuple.x, tuple.y);
+		public static bool operator ==(Vec2 a, Vec2 b) => a.Equals(b);
+		public static bool operator !=(Vec2 a, Vec2 b) => !a.Equals(b);
+		public bool Equals(Vec2 other) => other.X == X && other.Y == Y;
+		public bool ApproxEquals(Vec2 other, float epsilon = epsilon) =>
+			MathF.Abs(other.X - X) < epsilon && MathF.Abs(other.Y - Y) < epsilon;
+		public override bool Equals(object obj) => obj is Vec2 vec2 && Equals(vec2);
+		public override int GetHashCode() => HashCode.Combine(X, Y);
 		public override string ToString() => $"({X},{Y})";
 		public float LengthSquared() => X * X + Y * Y;
 		public float Length() => MathF.Sqrt(LengthSquared());
